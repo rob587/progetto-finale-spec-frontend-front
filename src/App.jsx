44938210dd0b0d075da7d1c5e0d0f5ccc04d7f86
rob587 <robertocamm02@yahoo.com>
2026-01-15 12:57:06 +0100

    feat: add Comparatore page and update NavigationBar with Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import DefaultLayout from "./layout/DefaultLayout";
 import Favourites from "./pages/Favourites";
 import DetailGame from "./pages/DetailGame";
 import Homepage from "./pages/Homepage";
+import Comparatore from "./pages/Comparatore";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route index element={<Homepage />} />
           <Route path="favourites" element={<Favourites />} />
           <Route path="game/:id" element={<DetailGame />} />
+          <Route path="diff" element={<Comparatore />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const NavigationBar = () => {
-  return <div></div>;
+  return (
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand>Store</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="favourites">Preferiti</Nav.Link>
+            <Nav.Link href="diff">Comparatore</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-3"
+              aria-label="Search"
+            />
+            <Button variant="success">Cerca</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavigationBar;
