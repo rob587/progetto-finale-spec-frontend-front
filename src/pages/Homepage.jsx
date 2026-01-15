efae5f8f1af5de97949b61fce538ed8283c916aa
rob587 <robertocamm02@yahoo.com>
2026-01-15 15:01:25 +0100

    feat: implement Homepage with Bootstrap Card components

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Card, Container, Row, Col, Button } from "react-bootstrap";
 
 const Homepage = () => {
-  return <h1>ciao</h1>;
+  return (
+    <Container className="my-4">
+      <Row xs={1} md={2} lg={3} className="g-4">
+        {[...Array(15)].map((_, i) => (
+          <Col key={i}>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title {i + 1}</Card.Title>
+                <Card.Text>
+                  This is card number {i + 1} with some supporting text below as
+                  a natural lead-in to additional content.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <Button href="game/:id">Link</Button>
+              </Card.Footer>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 };
 
 export default Homepage;
